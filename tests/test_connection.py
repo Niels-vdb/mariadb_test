99b1e112e28f358f51f79187769e5d637014d01d
Niels-vdb <nielsvdbosch95@gmail.com>
2024-06-07 15:33:26 +0200

    UPDATE: fixed faulty tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,30 +1,27 @@
 import pytest
 import sys
 import pathlib
-import mariadb
 
 root_dir = str(pathlib.Path(__file__).resolve().parents[1])
 sys.path.append(root_dir)
 
 my_path = sys.path
 
-from connection import connection
+from services.database.Database import Database
+
+database = Database()
 
 def test_create_connection():
     username = 'myadmin'
     password = 'password'
-    ip_address = '127.0.0.1'
-    database_name = 'test_db'
 
-    conn = connection(username, password, ip_address, database_name)
+    conn = database.connection(username, password)
 
-    assert conn == True
+    assert conn == None
 
 def test_create_connection_invalid():
     username = 'falseadmin'
     password = 'password'
-    ip_address = '127.0.0.1'
-    database_name = 'test_db'
 
     with pytest.raises(SystemExit):
-        conn = connection(username, password, ip_address, database_name)
+        database.connection(username, password)
